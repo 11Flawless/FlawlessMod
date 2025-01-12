adf74b16d3ffc73c93e7db4f3aa9d86a097e7994
rainsbc22 <158375365+rainsbc22@users.noreply.github.com>
2025-01-13 03:29:10 +1300

    pink gem

--- src/main/java/net/flawless/tutorialmod/TutorialMod.java
+++ src/main/java/net/flawless/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.flawless.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.flawless.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
